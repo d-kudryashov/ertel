d45f39d60b222e71a2b01f8973aaea7e684bc524
d.kudryashov <d.kudryashov2014@yandex.ru>
2018-05-22 18:44:01 +0500

    Spring Boot and MVC prepared

---
+++ src/main/java/erteltest/MVCConfiguration.java
@@ -0,0 +1,11 @@
+package erteltest;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class MVCConfiguration extends WebMvcAutoConfiguration {
+
+}
